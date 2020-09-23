f4ad41dc7842ebe55daf5b8e79b3dc23446826b7
zhengzhaosheng <1350111595@qq.com>
2020-09-23 23:06:44 +0800

    新建工程，初次提交。

---
+++ src/main/java/com/smallmorning/zwebservice/ZwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smallmorning.zwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smallmorning/zwebservice/ZwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallmorning.zwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
