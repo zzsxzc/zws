27c58470deb6d68aa102caa2a07892dd409e018a
zzsxzc <1350111595@qq.com>
2020-12-26 09:14:27 +0800

    templates写成了tenplate，浪费了很多时间

--- src/main/java/com/smallmorning/zwebservice/controller/HomeController.java
+++ src/main/java/com/smallmorning/zwebservice/controller/HomeController.java
@@ -1,22 +1,20 @@
 package com.smallmorning.zwebservice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
-*@Author zheng zs
-*@Date 21:24 2020/9/11
-*@Description 首页控制器
-**/
+ * 首页控制器
+ */
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     public String index(){
-        return "HomePage-zws";
+        return "index";
     }
 
 }
