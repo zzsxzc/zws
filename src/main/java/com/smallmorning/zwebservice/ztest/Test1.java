f11da90ba96338748c4f47f642103873329e3c4c
zhengzhaosheng <1350111595@qq.com>
2020-12-28 21:21:25 +0800

    更新网站首页

--- src/main/java/com/smallmorning/zwebservice/ztest/Test1.java
+++ src/main/java/com/smallmorning/zwebservice/ztest/Test1.java
@@ -6,8 +6,7 @@ import java.util.Date;
 
 public class Test1 {
     public static void main(String[] args) {
-        UserBase user_base = new UserBase(1,"zzs",new Date());
-        System.out.println(user_base.toString());
+
     }
 
 
