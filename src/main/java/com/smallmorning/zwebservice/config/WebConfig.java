455d43759ee169e726df08c22ea11b9a5a000717
zzsxzc <1350111595@qq.com>
2021-03-08 16:14:31 +0800

    基本完成，服务器配置数据库即可

--- src/main/java/com/smallmorning/zwebservice/config/WebConfig.java
+++ src/main/java/com/smallmorning/zwebservice/config/WebConfig.java
@@ -10,14 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         //本机
-        registry.addResourceHandler("/desurl/**").addResourceLocations("file:D:/apprep/image/");
+        //registry.addResourceHandler("/desurl/**").addResourceLocations("file:D:/apprep/image/");
         //下载类的需要这个模板，非下载类的不需要转换
-        registry.addResourceHandler("/apppackageurl/**").addResourceLocations("file:D:/apprep/package/");
+        //registry.addResourceHandler("/apppackageurl/**").addResourceLocations("file:D:/apprep/package/");
 
         //云主机
         //Linux
-        //registry.addResourceHandler("/desurl/**").addResourceLocations("file:/apprep/image/");
-        //registry.addResourceHandler("/apppackageurl/**").addResourceLocations("file:/apprep/package/");
+        registry.addResourceHandler("/desurl/**").addResourceLocations("file:/apprep/image/");
+        registry.addResourceHandler("/apppackageurl/**").addResourceLocations("file:/apprep/package/");
 
         //windows
         //registry.addResourceHandler("/videos/**").addResourceLocations("file:C:/videos/sh/");
